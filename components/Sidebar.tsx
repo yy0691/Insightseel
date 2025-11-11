c546e19c5a306d7fea8fb8afe0c59bff3f29d5b0
yuyuan-cs <116480542+yuyuan-cs@users.noreply.github.com>
2025-11-11 16:59:20 +0800

    Updated Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -28,7 +28,8 @@ const VideoItem: React.FC<{ video: Video; selectedVideoId: string | null; onSele
         className={`${commonClasses} ${isSelected ? selectedClasses : hoverClasses} ${isCollapsed ? 'justify-center' : ''}`}
       >
         <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 flex-shrink-0 ${isSelected ? 'text-slate-800' : 'text-slate-500'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z" />
         </svg>
         {!isCollapsed && (
           <span className="ml-3 text-sm truncate">{video.name}</span>
